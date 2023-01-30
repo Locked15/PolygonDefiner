b2ce1d0c6a5f6d95fcc4ae5bdeeeb9a2dc3fae05
Locked15 <almgal353@gmail.com>
2023-01-30 23:17:43 +0500

    Line model class created.

---
+++ PolygonDefiner/Models/Line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonDefiner.Models
+{
+    internal class Line
+    {
+        public Point StartPoint { get; private set; }
+
+        public Point EndPoint { get; private set; }
+
+        public Line(Point start, Point end) 
+        { 
+            StartPoint = start;
+            EndPoint = end;
+        }
+    }
+}
