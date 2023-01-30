8ecc02c2d93d66fcb5c6a2acd7f7b04e362ebf87
Locked15 <almgal353@gmail.com>
2023-01-30 23:17:55 +0500

    Point model class created.

---
+++ PolygonDefiner/Models/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace PolygonDefiner.Models
+{
+    internal class Point
+    {
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
